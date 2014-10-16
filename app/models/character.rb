66f3962904f4d38e96e45f0f238f1ad011d9ff99
Nosajool <jmloo@uwaterloo.ca>
2014-10-15 21:03:27 -0400

    win loss for characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -12,4 +12,12 @@ class Character < ActiveRecord::Base
 	def self.matches(character_id)
 		Match.where("wcharacter_id = #{character_id} OR lcharacter_id = #{character_id}")
 	end
+
+	def self.num_victories(character_id)
+		Match.where(wcharacter_id: character_id).count
+	end
+
+	def self.num_defeats(character_id)
+		Match.where(lcharacter_id: character_id).count
+	end
 end
